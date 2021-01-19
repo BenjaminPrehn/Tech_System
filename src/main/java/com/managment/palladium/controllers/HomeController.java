2d7a0120560e224f71fb6dcaf295ef3df8292c37
BenjaminPrehn <57618388+BenjaminPrehn@users.noreply.github.com>
2021-01-19 17:29:45 +0100

    failed Push

--- src/main/java/com/managment/palladium/controllers/HomeController.java
+++ src/main/java/com/managment/palladium/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.managment.palladium.controllers;
 
 import com.managment.palladium.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +10,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @Value("${version}")
+    private String ver;
+
     @Autowired
     EmployeeService empService;
 
     @GetMapping("/")
-    public String displayHome(){
+    public String displayHome(Model model){
+
+        model.addAttribute("versionNumber", ver);
 
         return "index";
     }
