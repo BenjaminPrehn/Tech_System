b5e1edc543c490264cd2c0b96827309ae715f3f6
BenjaminPrehn <57618388+BenjaminPrehn@users.noreply.github.com>
2021-01-25 10:11:52 +0100

    Test

--- src/test/java/com/managment/palladium/PalladiumApplicationTests.java
+++ src/test/java/com/managment/palladium/PalladiumApplicationTests.java
@@ -1,5 +1,7 @@
 package com.managment.palladium;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class PalladiumApplicationTests {
 
     @Test
     void contextLoads() {
+        String var = "hej";
+
+        assertEquals("hello",var);
     }
+
+
 
 }
