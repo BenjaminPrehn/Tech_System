98f1d9aac313baebc7b243230e6cf0f2e9bb8ce3
TheisSchlanderNielsen <57490253+TheisSchlanderNielsen@users.noreply.github.com>
2021-01-26 09:26:28 +0100

    test

---
+++ src/test/java/com/managment/palladium/RepositoryTests.java
@@ -0,0 +1,22 @@
+package com.managment.palladium;
+
+
+import com.managment.palladium.entities.Employee;
+import org.junit.*;
+import com.managment.palladium.dao.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RepositoryTests {
+
+    @Autowired
+    EmployeeRepository empRepo;
+
+
+
+
+    
+
+
+}
